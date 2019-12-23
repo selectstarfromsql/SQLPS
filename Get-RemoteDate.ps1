fd5a809bbffa2f4ca61585d0d75843f2325bf94b
yogigollapudi <yogigollapudi@gmail.com>
2019-12-23 16:00:10 -0600

    added all files from ps

---
+++ Get-RemoteDate.ps1
@@ -0,0 +1,41 @@
+﻿function Get-RemoteDate{
+    <#
+    .Author
+    Abdul Wajid
+    .SYNOPSIS
+    Queries a remote computer to get Current Date & Time including Time Zone information.
+    .DESCRIPTION
+    Queries a remote computer to get Current Date & Time including Time Zone information. Queries thorugh WMI so the WMI ports through the firewall should be open.
+    .PARAMTERER ComputerName
+    The Host Name or IP Address of the Computer
+    .EXAMPLE
+    .\Get-RemoteDate -Computername WHATEVER
+    This will query a remote computer named WHATEVER and fetch the required information
+    .EXAMPLE
+    .\Get-RemoteDate -ComputerName WHATEVER | Format-Table *
+    This will display the information in a tabular format.
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$True,
+                   ValueFromPipeLine=$True,
+                   ValueFromPipeLineByPropertyName=$True, 
+                   HelpMessage="ComputerName or IP Address to query via WMI")]
+        [string[]]$ComputerName
+    )
+
+    foreach($computer in $computerName)
+        {
+
+        $timeZone=Get-WmiObject -Class win32_timezone -ComputerName $computer
+        $localTime = Get-WmiObject -Class win32_localtime -ComputerName $computer
+        $output =@{'ComputerName' = $localTime.__SERVER;
+                    'Time Zone' = $timeZone.Caption;
+                    'Current Time' = (Get-Date -Day $localTime.Day -Month $localTime.Month);
+                   }
+        $object = New-Object -TypeName PSObject -Property $output
+        Write-Output $object
+
+        }
+
+        }
---
+++ get server local time and timezone.ps1
@@ -0,0 +1,16 @@
+﻿$servers = gc "C:\Users\ps317387\Desktop\sat 23.txt"
+
+ 
+    foreach($computer in $servers) 
+        { 
+ 
+        $timeZone=Get-WmiObject -Class win32_timezone -ComputerName $computer 
+        $localTime = Get-WmiObject -Class win32_localtime -ComputerName $computer 
+        $output =@{'ComputerName' = $localTime.__SERVER; 
+                    'Time Zone' = $timeZone.Caption; 
+                    'Current Time' = (Get-Date -Day $localTime.Day -Month $localTime.Month); 
+                   } 
+        $object = New-Object -TypeName PSObject -Property $output 
+        Write-Output $object 
+ 
+        } 
